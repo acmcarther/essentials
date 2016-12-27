77294071710f5845dbae563191fc30f958933ad5
Alex McArther <acmcarther@gmail.com>
2016-12-27 13:27:14 -0800

    Update version of grafana to 4.0.2: Alerting YAY

--- nix/server.nix
+++ nix/server.nix
@@ -31,6 +31,23 @@
       };
     });
 
+    grafana = pkgs.grafana.overrideDerivation( oldAttrs: rec {
+      version = "4.0.2";
+      ts = "1481203731";
+
+      src = pkgs.fetchFromGitHub {
+        rev = "v${version}";
+        owner = "grafana";
+        repo = "grafana";
+        sha256 = "1z71nb4qmp1qavsc101k86hc4yyis3mlqb1csrymkhgl94qpiiqm";
+      };
+
+      srcStatic = pkgs.fetchurl {
+        url = "https://grafanarel.s3.amazonaws.com/builds/grafana-${version}-${ts}.linux-x64.tar.gz";
+        sha256 = "1jnh2hn95r1ik0z31b4p0niq7apykppf8jcjjhsbqf8yp8i2b737";
+      };
+    });
+
     elasticsearch2 = pkgs.elasticsearch2.overrideDerivation( oldAttrs: {
       src = pkgs.fetchurl {
         url = "https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/2.4.0/elasticsearch-2.4.0.tar.gz";
